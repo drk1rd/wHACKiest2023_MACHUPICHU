9733ff20056fe2323bf1dc0935559d52a464849a
drk1rd <58465650+drk1rd@users.noreply.github.com>
2023-01-27 17:24:58 +0530

    Create bk.py

---
+++ temp/scripts/bk.py
@@ -0,0 +1,22 @@
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient('mongodb+srv://root:toor@cluster0.r8i6ufr.mongodb.net/?retryWrites=true&w=majority') #connects to database
+
+db=cluster['lmdb']
+collection=db['users']
+
+fb_user=collection.find({"sport":"Football"})
+bb_user=collection.find({"sport":"Basketball"})
+bd_user=collection.find({"sport":"Badminton"})
+
+football = []
+basketball = []
+badminton = []
+
+for x in fb_user:
+    football.append(x["name"])
+for x in bb_user:
+    basketball.append(x["name"])
+for x in bd_user:
+    badminton.append(x["name"])
