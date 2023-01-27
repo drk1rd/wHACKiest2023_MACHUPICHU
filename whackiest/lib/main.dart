584aebe228d9080ae089b5924e233b4a29f823c3
DhanrajRateria <116026855+DhanrajRateria@users.noreply.github.com>
2023-01-27 18:55:06 +0530

    flutter

---
+++ whackiest/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'chatwindow.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
+}
+
+// This widget is the home page of your application. It is stateful, meaning
+// that it has a State object (defined below) that contains fields that affect
+// how it looks.
+
+// This class is the configuration for the state. It holds the values (in this
+// case the title) provided by the parent (in this case the App widget) and
+// used by the build method of the State. Fields in a Widget subclass are
+// always marked "final".
+
+MaterialApp _gotochat() {
+  return MaterialApp(
+    home: ChatPage(),
+  );
+
+  // ignore: dead_code
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Next Match"),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _gotochat,
+        child: const Icon(Icons.chat),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
